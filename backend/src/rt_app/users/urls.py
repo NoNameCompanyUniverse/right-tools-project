f72e1768de149fb79a23e7a64055797f948c8fb5
Roman Sotnikov <romasotnikov12345@gmail.com>
2022-04-14 03:38:33 +0300

    add module users

--- backend/src/rt_app/users/urls.py
+++ backend/src/rt_app/users/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 
-urlpatterns = [
+from .views import *
 
+urlpatterns = [
+    path('me/', CurrentUserView.as_view({'get': 'retrieve'})),
+    path('', UserView.as_view({'get': 'list', 'post': 'create'})),
+    path('<int:pk>/', UserView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    path('<int:pk>/staff/', StaffView.as_view()),
+    path('<int:pk>/password/', PasswordView.as_view()),
+    path('<int:pk>/photo/', PhotoView.as_view()),
 ]
