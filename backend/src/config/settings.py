6b0ef0ea58d4fcd00aad95dcf6eb5d4e3241f769
Roman Sotnikov <axeman.ofic@gmail.com>
2022-06-06 20:37:41 +0300

    update settings.py

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -212,7 +212,7 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
+    "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
