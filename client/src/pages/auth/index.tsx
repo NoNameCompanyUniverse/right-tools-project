6ed27bcd53397d52971c052c5765dd229eb9148a
rTNY4eYati2% <s.maksimov@id-east.ru>
2022-06-06 17:32:36 +0300

    test auth

--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -24,7 +24,7 @@ const Auth = () => {
         await signIn('credentials', {
             username: auth.username,
             password: auth.password,
-            callbackUrl: '/'
+            callbackUrl: 'http://139.28.222.233/'
         })
     }
 
