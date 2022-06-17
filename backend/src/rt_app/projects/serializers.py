a038821d9ca5032f61ea640120955eb48ca2bece
Roman Sotnikov <axeman.ofic@gmail.com>
2022-06-17 23:43:38 +0300

    fix update and create project

--- backend/src/rt_app/projects/serializers.py
+++ backend/src/rt_app/projects/serializers.py
@@ -1,54 +1,45 @@
-from rest_framework.serializers import (
-    ModelSerializer,
-    ListSerializer,
-    SerializerMethodField,
-    ManyRelatedField,
-    PrimaryKeyRelatedField,
-    IntegerField,
-    Serializer
-)
-
-from rest_framework import fields
-
+from rest_framework import fields, serializers
 from rt_app.models import Project, User
 from rt_app.common.serializers import ServiceSerializer
 
 
-class UsersListSerializer(ServiceSerializer, ModelSerializer):
-    online = SerializerMethodField('is_online', label="Онлайн ли пользователь")
+class UsersListSerializer(ServiceSerializer, serializers.ModelSerializer):
+    online = serializers.SerializerMethodField('is_online', label="Онлайн ли пользователь")
 
     class Meta:
         model = User
         fields = ('id', 'username', 'online', 'last_login', 'photo')
 
 
-class ProjectListSerializer(ModelSerializer):
+class ProjectListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ('id', 'picture', 'name', 'date_create')
 
 
-class ProjectDetailSerializer(ModelSerializer):
-    participant = ListSerializer(child=UsersListSerializer())
+class ProjectDetailSerializer(serializers.ModelSerializer):
+    admin = UsersListSerializer()
 
     class Meta:
         model = Project
-        fields = '__all__'
+        exclude = ('participant',)
 
 
-class ProjectCreateUpdateSerializer(ModelSerializer):
-    participant = ListSerializer(child=IntegerField(), allow_empty=False, required=True)
+class ProjectCreateUpdateSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=150, required=True)
+    description = serializers.CharField(allow_null=False, required=False)
 
     def create(self, validated_data):
-        return Project.objects.create(**validated_data, user=self.context.get('user'))
+        admin = self.context.get('request').user
+        return Project.objects.create(**validated_data, admin=admin)
 
     class Meta:
         model = Project
-        exclude = ('admin',)
+        fields = ('id', 'name', 'description')
 
 
-class PictureSerializer(ModelSerializer):
-    picture = fields.ImageField(allow_null=True, max_length=100, required=False)
+class PictureSerializer(serializers.ModelSerializer):
+    picture = serializers.ImageField(allow_null=True, max_length=100, required=False)
 
     class Meta:
         model = Project
