9f2b7abe36109181c9fc8d98d8d1b58dd21c1f5f
Roman Sotnikov <axeman.ofic@gmail.com>
2022-06-22 15:55:26 +0300

    fix models

--- backend/src/rt_app/projects/kanban_boards/serializers.py
+++ backend/src/rt_app/projects/kanban_boards/serializers.py
@@ -4,19 +4,6 @@ from rt_app.models import KanbanBoard
 
 
 class KanbanBoardListCreateSerializer(serializers.ModelSerializer):
-    original_background_url = serializers.URLField(allow_null=False, required=True)
-    compress_background_url = serializers.URLField(allow_null=False, required=True)
-
-    def validate_original_background_url(self, value):
-        if 'https://images.pexels.com/photos/' in value:
-            return value
-        raise serializers.ValidationError('Адрес картинки должен быть взят с https://images.pexels.com/')
-
-    def validate_compress_background_url(self, value):
-        if 'https://images.pexels.com/photos/' in value:
-            return value
-        raise serializers.ValidationError('Адрес картинки должен быть взят с https://images.pexels.com/')
-
     def create(self, validated_data):
         project = self.context.get('view').get_object()
         return KanbanBoard.objects.create(**validated_data, project=project)
--- backend/src/rt_app/projects/mind_maps/serializers.py
+++ backend/src/rt_app/projects/mind_maps/serializers.py
@@ -4,11 +4,6 @@ from rt_app.models import MindMap
 
 
 class MindMapListCreateSerializer(serializers.ModelSerializer):
-    def validate_compress_background_url(self, value):
-        if 'https://images.pexels.com/photos/' in value:
-            return value
-        raise serializers.ValidationError('Адрес картинки должен быть взят с https://images.pexels.com/')
-
     def create(self, validated_data):
         project = self.context.get('view').get_object()
         return MindMap.objects.create(**validated_data, project=project)
