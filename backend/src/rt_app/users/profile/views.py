6941abf6959a3cd8d259302910f4b2f86480d15d
Roman Sotnikov <axeman.ofic@gmail.com>
2022-06-26 18:34:05 +0300

    add detail info user

--- backend/src/rt_app/users/profile/views.py
+++ backend/src/rt_app/users/profile/views.py
@@ -1,21 +1,30 @@
+from django.shortcuts import Http404
 from django.db.models import Q
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
 from rest_framework import views, status, generics
 
-from rt_app.models import Project
+from rt_app.models import Project, User
 
 from rt_app.projects.serializers import ProjectListSerializer
 
 from .serializers import *
 
 
-class ProfileInfoView(views.APIView):
-    def get_object(self):
-        return self.request.user
+class BaseUserView:
+    def get_object(self, pk=None):
+        if pk:
+            try:
+                return User.objects.get(pk=pk)
+            except User.DoesNotExist:
+                raise Http404
+        else:
+            return self.request.user
 
+
+class ProfileInfoView(BaseUserView, views.APIView):
     @swagger_auto_schema(
-        tags=['Текущий пользователь'],
+        tags=['Текущий пользователь', 'Пользователи'],
         operation_summary="Информация на главной странице",
         responses={
             200: ProfileInfoSerializer(read_only=True),
@@ -23,25 +32,26 @@ class ProfileInfoView(views.APIView):
             404: 'Not found',
         }
     )
-    def get(self, request):
-        user = self.get_object()
+    def get(self, request, *args, **kwargs):
+        user = self.get_object(pk=kwargs.get('pk'))
         serializer = ProfileInfoSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class ProfileProjectsView(generics.ListAPIView):
+class ProfileProjectsView(BaseUserView, generics.ListAPIView):
     serializer_class = ProjectListSerializer
 
     def get_queryset(self):
+        user = self.get_object(pk=self.kwargs.get('pk'))
         return Project.objects\
             .select_related('admin')\
             .prefetch_related('participant')\
-            .filter(Q(admin=self.request.user) | Q(participant__in=[self.request.user]))\
+            .filter(Q(admin=user) | Q(participant__in=[user]))\
             .distinct()\
             .order_by('pk')
 
     @swagger_auto_schema(
-        tags=['Текущий пользователь'],
+        tags=['Текущий пользователь', 'Пользователи'],
         operation_summary="Информация проектах для данного пользователя",
         responses={
             200: ProjectListSerializer(many=True),
--- backend/src/rt_app/users/urls.py
+++ backend/src/rt_app/users/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('me/profile/', include('rt_app.users.profile.urls')),
     path('', UserView.as_view({'get': 'list', 'post': 'create'})),
     path('<int:pk>/', UserView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    path('<int:pk>/profile/', include('rt_app.users.profile.urls')),
     path('<int:pk>/staff/', StaffView.as_view()),
     path('<int:pk>/password/', PasswordView.as_view()),
     path('<int:pk>/photo/', PhotoView.as_view()),
